d219400760cbf154fb89785ba2f7b5943027603f
Deepal Suranga Weerasuriya <deepal.suranga29@gmail.com>
2015-12-22 11:42:18 +0530

    project start. create None helo world ->Hello! run it. no error. output:Rename Project (SystemManager_Project) 100% OK

---
+++ SystemManager_Project/src/main_view.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author deepal_suranga
+ */
+public class main_view {
+    
+    public static void main(String[] args) {
+       main_view.log("hello");
+    }
+    
+    public static String log(String message){
+        System.out.println(message);
+        
+        return message;
+    }
+    
+}
